cf76533dfd823721ee45a413e2bac9fb9eb09b73
Florian Engelhardt <florian.engelhardt@bergfreunde.de>
2019-04-15 14:38:54 +0200

    add simple products endpoint with app, composer deps and dockerfile

---
+++ products/app.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use Psr\Http\Message\ServerRequestInterface;
+use React\EventLoop\Factory;
+use React\Http\Response;
+use React\Http\Server;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loop = Factory::create();
+$server = new Server(function (ServerRequestInterface $request) {
+    return new Response(
+        200,
+        array(
+            'Content-Type' => 'application/json'
+        ),
+        json_encode([
+            'id' => 1,
+            'name' => 'Avengers',
+            'description' => 'a movie'
+        ])
+    );
+});
+$socket = new \React\Socket\Server('0.0.0.0:3000', $loop);
+$server->listen($socket);
+echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . PHP_EOL;
+$loop->run();
